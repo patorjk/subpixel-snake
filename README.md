fc7aac7afda76be8bebf0e769b1c9dc2040d6bb1
patorjk <patorjk@gmail.com>
2025-01-27 21:24:52 -0500

    updates

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 A game of Snake that uses subpixels.
 
 This is a fork of this game: https://github.com/patorjk/JavaScript-Snake
+
+## Running
+
+Clone project, then at command line:
+
+```
+npx parcel src/index.html
+```
+
+Runs on http://localhost:1234
+
+## Building
+
+```
+npm run build
+```
--- src/js/snake.js
+++ src/js/snake.js
@@ -612,36 +612,6 @@ SNAKE.Board = SNAKE.Board || (function () {
     return (highestIndex + 1);
   }
 
-  /*
-      This function returns the width of the available screen real estate that we have
-  */
-  function getClientWidth() {
-    let myWidth = 0;
-    if (typeof window.innerWidth === "number") {
-      myWidth = window.innerWidth;//Non-IE
-    } else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
-      myWidth = document.documentElement.clientWidth;//IE 6+ in 'standards compliant mode'
-    } else if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
-      myWidth = document.body.clientWidth;//IE 4 compatible
-    }
-    return myWidth;
-  }
-
-  /*
-      This function returns the height of the available screen real estate that we have
-  */
-  function getClientHeight() {
-    let myHeight = 0;
-    if (typeof window.innerHeight === "number") {
-      myHeight = window.innerHeight;//Non-IE
-    } else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
-      myHeight = document.documentElement.clientHeight;//IE 6+ in 'standards compliant mode'
-    } else if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
-      myHeight = document.body.clientHeight;//IE 4 compatible
-    }
-    return myHeight;
-  }
-
   // -------------------------------------------------------------------------
   // Contructor + public and private definitions
   // -------------------------------------------------------------------------
